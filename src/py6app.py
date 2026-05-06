44259c862d446c955d16d1946069148d9b3edc1f
aumthakkar <aumthakkar@yahoo.co.uk>
2026-05-06 16:49:50 +0100

    initial commit

---
+++ src/py6app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import time
+import socket
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+       "time": time.ctime(),
+       "host": socket.gethostname(),
+        "env": "dev",
+        "msg": "Hi Pranav, how are you?"
+    })
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+       "status": "up",
+
+    })
+    
+if __name__ == '__main__':
+    app.run("0.0.0.0")
